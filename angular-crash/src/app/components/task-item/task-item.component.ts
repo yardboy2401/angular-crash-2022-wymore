cabf99aa1b36acc44b5829d6899fbdaa3c2337a4
Jeff Wymore <yardboy2401@gmail.com>
2022-05-23 19:25:27 -0700

    delete tasks complete and task reminder toggling complete

--- angular-crash/src/app/components/task-item/task-item.component.ts
+++ angular-crash/src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {Task} from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,12 +8,23 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent implements OnInit {
-  @Input() task: Task
+  @Input() task: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter()
+
   faTimes = faTimes;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
   }
 
 }
